b09227e373566cf9212696d2d82c92373c894761
Junacik99 <junacik99@gmail.com>
2023-04-08 14:26:36 +0200

    read file

---
+++ parkmeans.cc
@@ -0,0 +1,48 @@
+/********************
+*   Martin Takacs   *
+*     xtakac07      *
+*    PRL project    *
+*  Parallel K-Means *
+********************/
+
+#include <iostream>
+#include <mpi.h>
+#include <vector>
+#include <fstream>
+#include <string>
+
+int main(int argc, char** argv){
+    MPI_Init(&argc, &argv);
+
+    // Read input from a file
+	const std::string filename = "numbers";
+	std::ifstream file(filename);
+	std::string line;
+	std::getline(file, line);
+
+	// Init buffer
+	std::vector<int> sendbuf;
+
+	// Convert to numbers
+	for (char c : line) {
+		sendbuf.push_back(static_cast<unsigned char>(c));
+	}
+
+	// int n = sendbuf.size();
+
+	int rank, size;
+
+		
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+
+
+
+
+
+
+    MPI_Finalize();
+
+    return 0;
+}
