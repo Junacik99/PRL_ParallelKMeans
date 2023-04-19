e61f7b213c026ccd4abcc3be29815999a12c389e
Junacik99 <junacik99@gmail.com>
2023-04-19 12:03:03 +0200

    points2cluster

--- parkmeans.cc
+++ parkmeans.cc
@@ -12,6 +12,16 @@
 #include <string>
 #include <cmath>
 
+// TODO: prerobit classu, aby zodpovedala c++ praktikam
+class Cluster {
+public:
+	float mean;
+	std::vector<int> points;
+	float prevMean;
+};
+
+void points2cluster(std::vector<int> buffer, int clusterCount, Cluster clusters[]);
+
 int main(int argc, char** argv){
     MPI_Init(&argc, &argv);
 
@@ -40,18 +50,14 @@ int main(int argc, char** argv){
 	// 1. Select 4 random distinct numbers (cluster means)
 	// We know that input is an array of random numbers (at least 4)
 	// so we can just take the first 4 numbers as cluster means
-	struct 
-	{
-		float mean;
-		std::vector<int> points;
-		int size = 0;
-	}clusters[clusterCount];
+	Cluster clusters[clusterCount];
 	
-	
-	int means[clusterCount];
+	// Assign and broadcast means	
 	for (int i=0; i<clusterCount; ++i){
 		clusters[i].mean = (float)sendbuf[i];
+		clusters[i].prevMean = clusters[i].mean;
 		MPI_Bcast(&clusters[i].mean, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
+		MPI_Bcast(&clusters[i].prevMean, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
 	}
 
 	// Create buffer for each processor and scatter data
@@ -59,24 +65,10 @@ int main(int argc, char** argv){
 	std::vector<int> recvbuf(subArrSize);
 	MPI_Scatter(sendbuf.data(), subArrSize, MPI_INT, recvbuf.data(), subArrSize, MPI_INT, 0, MPI_COMM_WORLD);
 	
-
 	// 2. Calculate distances for every other point
 	// 3. Assign every point to the nearest cluster
-	for (int x : recvbuf){
-		// Find min distance from clusters means
-		int cIndex = 0;
-		float minDist = MAXFLOAT;
-		for (int i=0; i<clusterCount; ++i){
-			float dist = std::abs(x - clusters[i].mean);
-			if (dist < minDist){
-				minDist = dist;
-				cIndex = i;
-			}
-		}
-		// Assign point to the cluster
-		clusters[cIndex].points.push_back(x);
-		clusters[cIndex].size++;
-	}
+	points2cluster(recvbuf, clusterCount, clusters);
+
 
 	// Merge clusters from each processor
 	for (int c=0; c<clusterCount; ++c){
@@ -104,6 +96,18 @@ int main(int argc, char** argv){
 			for (int x : joinedCluster)
 				std::cout<< x << " ";
 			std::cout<< std::endl;
+			
+			// 4. Calculate new mean for every cluster
+			int sum = 0;
+			for (int x : joinedCluster)
+				sum += x;
+			clusters[c].mean = (float)sum / joinedCluster.size();
+
+			// if (clusters[c].mean != clusters[c].prevMean)
+			// 	sameMeans = false;
+
+			// clusters[c].prevMean = clusters[c].mean;
+
 		}
 		else{
 			// Send points to the root process
@@ -112,31 +116,44 @@ int main(int argc, char** argv){
 		}
 	}
 
-
-// if (rank==1){
-	// std::cout << "Processor " << rank << std::endl;
-//     // for (int x : recvbuf)
-//     //     std::cout << x << " ";
-//     // std::cout<<std::endl;
-// 	// std::cout << "means: ";
-// 	// for (int i = 0; i<clusterCount; ++i)
-// 	// 	std::cout << clusters[i].mean << " ";
-// 	// std::cout<<std::endl;
-	// for (int i=0; i<clusterCount; ++i){
-	// 	std::cout << "Cluster["<<i<<"]: mean: "<<clusters[i].mean<< " \tpoints: ";
-	// 	for (int x : clusters[i].points)
-	// 		std::cout << x << " ";
-	// 	std::cout << "\tsize: "<<clusters[i].size;
-	// 	std::cout<<std::endl;
-	// }
-// }
-
-	// 4. Calculate new mean for every cluster
-
+	for(int i=0; i<clusterCount; ++i){
+		// Broadcast new means
+		MPI_Bcast(&clusters[i].mean, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
+		MPI_Bcast(&clusters[i].prevMean, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
+		// clear current cluster vectors
+		clusters[i].points.clear();
+	}
 
 	// 5. Calculate distances for each point to every cluster
 	// 6. Reassign points to their respective cluster
+	points2cluster(recvbuf, clusterCount, clusters);
+
+
 	// 7. Repeat from step 4. until means remain the same
+
+
+
+
+if (rank==0){
+	std::cout << "Processor " << rank << std::endl;
+//     // for (int x : recvbuf)
+//     //     std::cout << x << " ";
+//     // std::cout<<std::endl;
+	// std::cout << "means: ";
+	// for (int i = 0; i<clusterCount; ++i)
+	// 	std::cout << clusters[i].mean << " ";
+	// std::cout<<std::endl;
+	for (int i=0; i<clusterCount; ++i){
+		std::cout << "Cluster["<<i<<"]: mean: "<<clusters[i].mean<<"\tprevMean: "<<clusters[i].prevMean << " \tpoints: ";
+		for (int x : clusters[i].points)
+			std::cout << x << " ";
+		std::cout << "\tsize: "<<clusters[i].points.size();
+		std::cout<<std::endl;
+	}
+}
+
+
+
 
 
 
@@ -149,4 +166,21 @@ int main(int argc, char** argv){
     MPI_Finalize();
 
     return 0;
+}
+
+void points2cluster(std::vector<int> buffer, int clusterCount, Cluster clusters[]){
+	for (int x : buffer){
+		// Find min distance from clusters means
+		int cIndex = 0;
+		float minDist = MAXFLOAT;
+		for (int i=0; i<clusterCount; ++i){
+			float dist = std::abs(x - clusters[i].mean);
+			if (dist < minDist){
+				minDist = dist;
+				cIndex = i;
+			}
+		}
+		// Assign point to the cluster
+		clusters[cIndex].points.push_back(x);
+	}
 }
